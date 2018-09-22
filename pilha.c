6e34652c2b65b29f9977865e4518a61a07e26f0b
antoniobazzarella <antoniobazzarellamp@gmail.com>
2018-09-22 11:32:07 -0300

    Está sendo feito a atualização dos dados.
    RETOMAR DAQUI!

--- pilha.c
+++ pilha.c
@@ -2,39 +2,83 @@
 #include <stdlib.h>
 #include "pilha.h"
 
-typedef struct no {
-  Token token;
-  struct no *prox;
-} No;
+typedef struct pilha{
+	Token *dados;
+	int tamanho;
+	int topo;
+} Pilha;
 
-struct pilha {
-  No *primeiro;
-};
+Pilha* pilha_criar(int tamanho){
+	Pilha *p = (Pilha*) malloc(sizeof(Pilha));
+	p->topo = 0;
+	p->dados = (double*) malloc(tamanho*sizeof(double));
+	p->tamanho = tamanho;
+	return p;
+}
+	
+void pilha_push(Pilha *p, Token t)
+{
 
-Pilha* pilha_criar() {
-	//Implemente
+	if(p->topo >= p->tamanho) {
+		printf("Pilha cheia!\n");
+		return;
+	}
+	p->dados[p->topo] = t;
+	p->topo++;
 }
 
-void pilha_push(Pilha *p, Token t) {
-	//Implemente
+Token pilha_pop(Pilha *p){
+
+	if(p->topo == 0) {
+		printf("Pilha vazia!\n");
+		Token t;
+		return t;
+	}
+	p->topo--;
+	return p->dados[p->topo];
 }
 
-Token pilha_pop(Pilha *p) {
-	//Implemente
+Token pilha_primeiro(Pilha *p){
+	
+	return p->dados[p->Token]
 }
-
-Token pilha_primeiro(Pilha *p) {
-	//Implemente
+int pilha_vazia(Pilha *p){
+	
+	if(Pilha->prox == NULL)
+		return 1;
+	else
+		return 0;	
+		
 }
+void pilha_destruir(Pilha *p){
 
-int pilha_vazia(Pilha *p) {
-	//Implemente
+	free(p->dados);
+	free(p);
 }
+void pilha_imprimir(Pilha *p)
+{
+	if(vazia(p))
+	{
+		printf("PILHA vazia!\n\n");
+	return ;
+	}
 
-void pilha_destruir(Pilha *p) {
-	//Implemente
-}
-
-void pilha_imprimir(Pilha *p) {
-	//Implemente
+	node *tmp;
+	tmp = p->prox;
+	printf("PILHA:");
+	while( tmp != NULL)
+	{
+		printf("%5d", tmp->num);
+		tmp = tmp->prox;
+	}
+	printf("\n        ");
+	int count;
+	for(count=0 ; count < tam ; count++)
+		printf("  ^  ");
+		printf("\nOrdem:");
+	for(count=0 ; count < tam ; count++)
+		printf("%5d", count+1);
+	
+	
+	 printf("\n\n");
 }
