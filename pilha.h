48794e6273b887e64a7d08abd8eeba0d1e81435f
Vitória Usuários Laboratório <lab-vitoria@laboratorios.corp>
2018-09-22 09:30:48 -0300

    Foram adicionados comandos para pilha.h

--- pilha.h
+++ pilha.h
@@ -3,41 +3,84 @@
 
 #include "tokenizacao.h"
 
-typedef struct pilha Pilha;
 
-/*
- * Cria uma nova pilha vazia.
- */
-Pilha* pilha_criar();
+typedef struct pilha{
+	double *dados;
+	int tamanho;
+	int topo;
+} Pilha;
 
-/*
- * Adiciona elemento no topo da pilha.
- */
-void pilha_push(Pilha *p, Token t);
+Pilha* pilha_criar(int tamanho){
+	Pilha *p = (Pilha*) malloc(sizeof(Pilha));
+	p->topo = 0;
+	p->dados = (double*) malloc(tamanho*sizeof(double));
+	p->tamanho = tamanho;
+	return p;
+}
+	
+void pilha_push(Pilha *p, Token t){
 
-/*
- * Retira elemento do início da pilha.
- */
-Token pilha_pop(Pilha *p);
+	if(p->topo >= p->tamanho) {
+		printf("Pilha cheia!\n");
+		return;
+	}
+	p->dados[p->topo] = info;
+	p->topo++;
+}
 
-/*
- * Retorna o primeiro elemento da pilha, sem retirá-lo.
- */
-Token pilha_primeiro(Pilha *p);
+Token pilha_pop(Pilha *p){
 
-/*
- * Retorna 1 se a pilha estiver vazia ou 0 caso contrário.
- */
-int pilha_vazia(Pilha *p);
+	if(p->topo == 0) {
+		printf("Pilha vazia!\n");
+		return 0;
+	}
+	p->topo--;
+	return p->dados[p->topo];
+}
 
-/*
- * Destrói a pilha e seus elementos.
- */
-void pilha_destruir(Pilha *p);
+Token pilha_primeiro(Pilha *p){
+	
+	return p->Pilha->Token;
+}
+int pilha_vazia(Pilha *p){
+	
+	if(Pilha->prox == NULL)
+		return 1;
+	else
+		return 0;	
+		
+}
+void pilha_destruir(Pilha *p){
 
-/*
- * Imprime todos os elementos na pilha atualmente.
- */
-void pilha_imprimir(Pilha *p);
+	free(p->dados);
+	free(p);
+}
+void pilha_imprimir(Pilha *p)
+{
+	if(vazia(Pilha))
+	{
+		printf("PILHA vazia!\n\n");
+	return ;
+	}
+
+	node *tmp;
+	tmp = Pilha->prox;
+	printf("PILHA:");
+	while( tmp != NULL)
+	{
+		printf("%5d", tmp->num);
+		tmp = tmp->prox;
+	}
+	printf("\n        ");
+	int count;
+	for(count=0 ; count < tam ; count++)
+		printf("  ^  ");
+		printf("\nOrdem:");
+	for(count=0 ; count < tam ; count++)
+		printf("%5d", count+1);
+	
+	
+	 printf("\n\n");
+}
 
 #endif
