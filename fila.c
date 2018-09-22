f413cb1bcd9fabde23ebfc1c83095aee6881c88d
antoniobazzarella <antoniobazzarellamp@gmail.com>
2018-09-22 11:31:31 -0300

    Foram corrigidos os dados conforme dicas do professor.

--- fila.c
+++ fila.c
@@ -3,34 +3,85 @@
 #include "fila.h"
 
 typedef struct no {
-  Token token;
-  struct no *prox;
+	Token t;
+	struct no *prox;
 } No;
-
-struct fila {
-  No *primeiro, *ultimo;
-};
+typedef struct fila {
+	struct no *inicio;
+	struct no *fim;
+} Fila;
 
 Fila* fila_criar() {
-	//Implemente
+	Fila *f = (Fila*) malloc(sizeof(Fila));
+	f->inicio = NULL;
+	f->fim = NULL;
+	return f;
 }
 
 void fila_adicionar(Fila *f, Token t) {
-	//Implemente
+No *n = (No*) malloc(sizeof(No));
+	n->t = t;
+	n->prox = NULL;
+	if(f->fim == NULL) {
+		f->inicio = n;
+		f->fim = n;
+		return 0;
+	}
+	f->fim->prox = n;
+	f->fim = n;
 }
 
 Token fila_remover(Fila *f) {
-	//Implemente
+	if(f->inicio == NULL) {
+		printf("Fila vazia!\n");
+		return;
+	}
+	No *excluir = f->inicio;
+	f->inicio = f->inicio->prox;
+	if(f->inicio == NULL) {
+		f->fim = NULL;
+	}
+	Token t = excluir->t;
+	free(excluir);
+	return t;
 }
 
 int fila_vazia(Fila *f) {
-	//Implemente
-}
+	if(f->inicio == NULL)
+		return 1;
+	else
+		return 0;
+	}
 
 void fila_destruir(Fila *f) {
-	//Implemente
+	if(!vazia(f)){
+		No *proxNode,
+			  *atual;
+
+		atual = f->inicio;
+		while(atual != NULL){
+			proxNode = atual->prox;
+			free(atual);
+			atual = proxNode;
+		}
+	}
 }
 
 void fila_imprimir(Fila *f) {
-	//Implemente
+	if(vazia(f)){
+		printf("Fila vazia!\n\n");
+		return ;
+	}
+
+	No *tmp;
+	tmp = f->inicio;
+	printf("Fila :");
+	while( tmp != NULL){
+		token_imprimir(tmp->t);
+		tmp = tmp->prox;
+	}
+	
+
+
+	printf("\n\n");
 }
